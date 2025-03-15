029e8dba727a8ba288b236be0d3fd7feeb53ec7b
Aleksandar Vujić <aleksandar.vujic@yandex.com>
2025-03-16 00:10:15 +0100

    Jobs app up and running

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# Job model
+class Job(models.Model):
+    image = models.ImageField(upload_to="images/")
+    summary = models.CharField(max_length=200)
+
+
+# Blog model
+class Blog(models.Model):
+    image = models.ImageField(upload_to="images/")
+    summary = models.TextField()
