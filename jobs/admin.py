6961e7a224cc1f5c95ad6fe8c48d63b078b93675
Aleksandar Vujić <aleksandar.vujic@yandex.com>
2025-03-25 13:29:08 +0100

    Fixed blog listing

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Job
-from .models import Blog
 
 admin.site.register(Job)
-
-admin.site.register(Blog)
--- jobs/models.py
+++ jobs/models.py
@@ -1,17 +1,5 @@
 from django.db import models
-from django.utils import timezone
 
-# Create your models here.
-
-# Job model
 class Job(models.Model):
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
-
-
-# Blog model
-class Blog(models.Model):
-    title = models.CharField(default="", max_length=255)
-    pubdate = models.DateTimeField(default=timezone.now)
-    body = models.TextField(default="")
-    image = models.ImageField(upload_to="images/")
